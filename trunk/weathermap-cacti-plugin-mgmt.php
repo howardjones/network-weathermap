8c21160deae8a53db4a53aa8cb0f99cb02719cf2
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2007-08-10 15:07:29 +0000

    More plug=1 integration

--- trunk/weathermap-cacti-plugin-mgmt.php
+++ trunk/weathermap-cacti-plugin-mgmt.php
@@ -147,7 +147,7 @@ default:
 	maplist();
 
 print '<br />'; 
-    html_start_box("<center><a target=\"_blank\" class=\"linkOverDark\" href=\"docs/\">Local Documentation</a> | <a target=\"_blank\" class=\"linkOverDark\" href=\"http://www.network-weathermap.com/\">Weathermap Website</a> | <a target=\"_target\" class=\"linkOverDark\" href=\"editor.php?plug=1\">Weathermap Editor</a> | This is version $WEATHERMAP_VERSION</center>", "78%", $colors["header"], "2", "center", "");
+    html_start_box("<center><a target=\"_blank\" class=\"linkOverDark\" href=\"docs/\">Local Documentation</a> -- <a target=\"_blank\" class=\"linkOverDark\" href=\"http://www.network-weathermap.com/\">Weathermap Website</a> -- <a target=\"_target\" class=\"linkOverDark\" href=\"editor.php?plug=1\">Weathermap Editor</a> -- This is version $WEATHERMAP_VERSION</center>", "78%", $colors["header"], "2", "center", "");
  html_end_box(); 
 
 

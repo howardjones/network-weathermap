5918158e9a484b8da8614c4a25b746275c9a4d3f
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2008-02-22 23:56:17 +0000

    Moved tests into tests/ folder. Neater.

---
+++ trunk/check-gdbug.php
@@ -0,0 +1,19 @@
+<?php
+
+	print "Let's see if you have the GD transparency bug...\n";
+
+	print "If you see no more output, then you do.";
+	print "You should either use PHP's built-in (aka 'bundled') GD library,\n";
+	print " or update to GD Version 2.0.34 or newer.\n\n";
+
+	$temp_width = 10;
+	$temp_height = 10;
+	
+	$node_im=imagecreatetruecolor($temp_width,$temp_height );
+	imageSaveAlpha($node_im, TRUE);
+	$nothing=imagecolorallocatealpha($node_im,128,0,0,127);
+	imagefill($node_im, 0, 0, $nothing);
+	imagedestroy($node_im);
+	
+	print "Nope, so that's good.\n";
+?>

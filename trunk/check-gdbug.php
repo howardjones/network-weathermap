8e03ba64ba83d4116b6e1589aeca3ecdc43b1c12
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2008-02-23 00:04:39 +0000

    Tidying up GD test stuff.

--- trunk/check-gdbug.php
+++ trunk/check-gdbug.php
@@ -1,10 +1,11 @@
 <?php
 
-	print "Let's see if you have the GD transparency bug...\n";
 
-	print "If you see no more output, then you do.";
 	print "You should either use PHP's built-in (aka 'bundled') GD library,\n";
 	print " or update to GD Version 2.0.34 or newer.\n\n";
+
+	print "Let's see if you have the GD transparency bug...\n";
+	print "If you see no more output, then you do.\n\n";
 
 	$temp_width = 10;
 	$temp_height = 10;
@@ -15,5 +16,5 @@
 	imagefill($node_im, 0, 0, $nothing);
 	imagedestroy($node_im);
 	
-	print "Nope, so that's good.\n";
+	print "Nope. We got past the risky part, so that's good.\nYour GD library look healthy.\n";
 ?>

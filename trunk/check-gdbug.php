7526e96d5595ac2f9a40b3ce2a77003753bc7a81
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2008-02-23 00:10:02 +0000

    More Tidying up GD test stuff.

--- trunk/check-gdbug.php
+++ trunk/check-gdbug.php
@@ -1,11 +1,13 @@
 <?php
+	print "Some version of the GD graphics library have a bug in their\nhandling of Alpha channels. Unfortunately, Weathermap uses these to draw Nodes.\n\n";
+	print "This program will test if your PHP installation is using a buggy GD library.\n";
 
-
-	print "You should either use PHP's built-in (aka 'bundled') GD library,\n";
-	print " or update to GD Version 2.0.34 or newer.\n\n";
+	print "If you are, you should either use PHP's built-in (aka 'bundled') GD library,\n";
+	print " or update to GD Version 2.0.34 or newer.\n\nWeathermap REQUIRES working Alpha support.\n\n";
 
 	print "Let's see if you have the GD transparency bug...\n";
-	print "If you see no more output, then you do.\n\n";
+	print "If you see no more output, or a segfault, then you do,\nand you'll need to upgrade.\n\n";
+	print "If you get other errors, like 'function not found', then run check.php to\nmake sure that your PHP installation is otherwise OK.\n\nHere we go...\n";
 
 	$temp_width = 10;
 	$temp_height = 10;
@@ -16,5 +18,5 @@
 	imagefill($node_im, 0, 0, $nothing);
 	imagedestroy($node_im);
 	
-	print "Nope. We got past the risky part, so that's good.\nYour GD library look healthy.\n";
+	print "Nope. We got past the risky part, so that's good.\nYour GD library looks healthy.\n";
 ?>

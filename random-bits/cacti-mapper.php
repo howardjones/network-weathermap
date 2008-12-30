1eca52b88f508e51fe7497dcc883065ffb4996d7
Howard Jones <howardjones@users.noreply.github.com>
2008-12-30 16:25:38 +0000

    Added the updated version of cacti-mapper.php

--- random-bits/cacti-mapper.php
+++ random-bits/cacti-mapper.php
@@ -4,7 +4,11 @@
 $cacti_base = '../../';
 $cacti_url = '/';
 
-include_once 'editor-config.php';
+$width = 4000;
+$height = 3000;
+
+
+require_once 'editor-config.php';
 
 // check if the goalposts have moved
 if( is_dir($cacti_base) && file_exists($cacti_base."/include/global.php") )
@@ -115,7 +119,7 @@ foreach($interfaces as $key=>$int)
 		$oid = ".1.3.6.1.2.1.4.20.1.3.".$int['ip'];
 		$hostid = $int['host'];
 		
-		if($count>-1) // old junk
+		if($count<100)
 		{
 			print "Fetching Netmask via SNMP for Host ".$int['host']."//".$int['ip']." from $oid\n";
 			$result = cacti_snmp_get(
@@ -167,15 +171,29 @@ print "Wrote $count cache entries.\n";
 $count=0;
 foreach($interfaces as $key=>$int)
 {
-	$network = get_network($int['ip'],$int['netmask'])."/".$int['netmask'];
+	if(isset($int['netmask']))
+	{
+	$network = get_network($int['ip'],$int['netmask'])."/".get_cidr($int['netmask']);
 	$interfaces[$key]['network'] = $network;
 	
 	$networks[$network] []= $key;
 	$count++;
+	}
+	else
+	{
+		print $int['ip']."\n";;
+	}
 }
 print "Assembled $count different network/netmask pairs\n";
 
+
+$link_config = "";
+$node_config = "";
+$nodes_seen = array();
+
 $count=0;
+$linkid = 0;
+$lannodeid = 0;
 foreach ($networks as $network=>$members)
 {
 	if(sizeof($members)<2)
@@ -193,48 +211,80 @@ foreach ($networks as $network=>$members)
 			print "  ".$interfaces[$int]['nicename'];
 			print " on ".$hosts[$h]['description'];
 			print " (".$hosts[$h]['hostname'].")\n";
+			$nodes_seen[$h]=1;
 		}
-		print "\n";
+		$linkid++;
+		$link_config .= "LINK link_$linkid\nWIDTH 4\n";
+		$link_config .= "\tNODES node_".$interfaces[$members[0]]['host']." node_".$interfaces[$members[1]]['host']."\n";
+		$link_config .= "\tSET in_interface ".$interfaces[$members[1]]['nicename']."\n";
+		$link_config .= "\tSET out_interface ".$interfaces[$members[0]]['nicename']."\n";
+		$link_config .=  "\n";
 	}
 	
 	if(sizeof($members)>2)
 	{
 		print "Create LAN NODE called $network and add LINKs from these NODEs to it:\n";
+		$x = rand(0,$width); $y = rand(0,$height);	
+		$lan_key = preg_replace("/[.\/]/","_",$network);
+		$node_config .= "NODE LAN_$lan_key\nLABELBGCOLOR 255 240 240 \n\tPOSITION $x $y\n\tLABEL $network\n\tICON 96 24 rbox\n\tLABELOFFSET C\n\tLABELOUTLINECOLOR none\nUSESCALE none in\n\n";
 		foreach($members as $int)
 		{
 			$h = $interfaces[$int]['host'];
 			print "  $int:: ".$interfaces[$int]['nicename'];
 			print " on ".$hosts[$h]['description'];
 			print " (".$hosts[$h]['hostname'].")\n";
+			$nodes_seen[$h]=1;
+			$linkid++;
+			$link_config .= "LINK link_$linkid\n";
+			$link_config .= "SET out_interface ".$interfaces[$int]['nicename']."\n";
+			$link_config .= "\tNODES node_$h LAN_$lan_key\n\tWIDTH 2\n\tOUTCOMMENT {link:this:out_interface}\n";
 		}
 		print "\n";
 	}
 }
 print "Trimmed $count networks with only one member interface\n";
 
+foreach ($nodes_seen as $h=>$c)
+{
+	$x = rand(0,$width); $y = rand(0,$height);	
+	$node_config .= "NODE node_$h\n\tSET cacti_id $h\n";
+	$node_config .= "\tLABEL ".$hosts[$h]['description']."\n";
+	$node_config .= "\tPOSITION $x $y\n";
+	$node_config .= "\tUSESCALE cactiupdown in \n";
+	$node_config .= "\tLABELFONTCOLOR contrast\n";
+	$node_config .= "\n\n";
+}
+
+$fd = fopen("automap.cfg","w");
+fputs($fd,"HTMLSTYLE overlib\nBGCOLOR 92 92 92\nWIDTH $width\nHEIGHT $height\nFONTDEFINE 30 GillSans 8\n");
+fputs($fd,"FONTDEFINE 20 GillSans 10\nFONTDEFINE 10 GillSans 9\n");
+fputs($fd,"SCALE DEFAULT 0 0 255 0 0\nSCALE DEFAULT 0 10   32 32 32   0 0 255\nSCALE DEFAULT 10 40   0 0 255   0 255 0\nSCALE DEFAULT 40 55   0 255 0   255 255 0\nSCALE DEFAULT 55 100   240 240 0   255 0 0\n");
+fputs($fd,"\nSCALE cactiupdown 0 0.5 192 192 192 \nSCALE cactiupdown 0.5 1.5 255 0 0 \nSCALE cactiupdown 1.5 2.5 0 0 255 \nSCALE cactiupdown 2.5 3.5 0 255 0 \nSCALE cactiupdown 3.5 4.5 255 255 0 \n");
+fputs($fd,"\nLINK DEFAULT\nBWSTYLE angled\nBWLABEL bits\nBWFONT 30\nCOMMENTFONT 30\n\n");
+fputs($fd,"\nNODE DEFAULT\nLABELFONT 10\n\n");
+fputs($fd,$node_config);
+fputs($fd,$link_config);
+fclose($fd);
 
 ///////////////////////////////////////////////////////////////
 
 function ip_to_int($_ip)
 {
-	$tok=strtok($_ip, ".");
-	$IPbit[]=$tok;
-
-	while ($tok)
+	if(preg_match("/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/",$_ip,$matches))
 	{
-		$tok=strtok(".");
-		$IPbit[]=$tok;
+		$_output=0;
+		for($i=1;$i<5;$i++)
+		{
+			$_output<<=8;
+			$_output+=$matches[$i];
+		}
+		return($_output);
 	}
-
-	$_output=0;
-
-	for ($i=0; $i < 4; $i++)
+	else
 	{
-		$_output<<=8;
-		$_output+=$IPbit[$i];
+		print "Something funny: $_ip\n";
+		return(-1);
 	}
-
-	return ($_output);
 }
 
 function int_to_ip($_int)
@@ -259,6 +309,40 @@ function get_network($_ip, $_mask)
 	$_network=$_int1 & ($_mask1);
 
 	return (int_to_ip($_network));
+}
+
+function get_cidr($mask)
+{
+	$lookup = array(
+		"255.255.255.255"=>"32",
+		"255.255.255.254"=>"31",
+		"255.255.255.252"=>"30",
+		"255.255.255.248"=>"29",
+		"255.255.255.240"=>"28",
+		"255.255.255.224"=>"27",
+		"255.255.255.192"=>"26",
+		"255.255.255.128"=>"25",
+		"255.255.255.0"=>"24",
+		"255.255.254.0"=>"23",
+		"255.255.252.0"=>"22",
+		"255.255.248.0"=>"21",
+		"255.255.240.0"=>"20",
+		"255.255.224.0"=>"19",
+		"255.255.192.0"=>"18",
+		"255.255.128.0"=>"17",
+		"255.255.0.0"=>"16",
+		"255.254.0.0"=>"15",
+		"255.252.0.0"=>"14",
+		"0.0.0.0.0"=>"0"
+	);
+
+	if($lookup[$mask]) return ($lookup[$mask]);
+
+	print "HUH: $mask\n";
+
+	return("-1");
+
+
 }
 
 

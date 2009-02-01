34dde0ba60459af5f1c1c30070b5bd3ae30f977d
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2009-02-01 22:32:44 +0000

    Removed reference to weathermap-cacti-plugin-editor.php for now.

--- trunk/weathermap-cacti-plugin-mgmt.php
+++ trunk/weathermap-cacti-plugin-mgmt.php
@@ -181,8 +181,8 @@ case 'addmap':
 	break;
 
 case 'editor':
-	chdir(dirname(__FILE__));
-	include_once('./weathermap-cacti-plugin-editor.php');
+	// chdir(dirname(__FILE__));
+	// include_once('./weathermap-cacti-plugin-editor.php');
 	break;
 
 case 'rebuildnow':
@@ -226,7 +226,7 @@ function weathermap_footer_links()
 	global $colors;
 	global $WEATHERMAP_VERSION;
 	print '<br />'; 
-    html_start_box("<center><a target=\"_blank\" class=\"linkOverDark\" href=\"docs/\">Local Documentation</a> -- <a target=\"_blank\" class=\"linkOverDark\" href=\"http://www.network-weathermap.com/\">Weathermap Website</a> -- <a target=\"_target\" class=\"linkOverDark\" href=\"weathermap-cacti-plugin-editor.php?plug=1\">Weathermap Editor</a> -- This is version $WEATHERMAP_VERSION</center>", "78%", $colors["header"], "2", "center", "");
+    html_start_box("<center><a target=\"_blank\" class=\"linkOverDark\" href=\"docs/\">Local Documentation</a> -- <a target=\"_blank\" class=\"linkOverDark\" href=\"http://www.network-weathermap.com/\">Weathermap Website</a> -- <a target=\"_target\" class=\"linkOverDark\" href=\"editor.php?plug=1\">Weathermap Editor</a> -- This is version $WEATHERMAP_VERSION</center>", "78%", $colors["header"], "2", "center", "");
 	html_end_box(); 
 }
 

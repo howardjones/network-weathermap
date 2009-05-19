ed8986bca46a463f8a20abee5e58af837e3e0f57
Howard Jones <howardjones@users.noreply.github.com>
2009-05-19 21:51:32 +0000

    Fix for PA 2.x and plugin presence checking

--- lib/datasources/WeatherMapDataSource_cactithold.php
+++ lib/datasources/WeatherMapDataSource_cactithold.php
@@ -34,13 +34,23 @@ class WeatherMapDataSource_cactithold extends WeatherMapDataSource {
 				debug('ReadData CactiTHold: Cacti database library not found. [THOLD001]\n');
 				return(FALSE);
 			}
-			
-			if( !isset($plugins) || !in_array('thold',$plugins))
+			if(function_exists("api_plugin_is_enabled"))
 			{
-				debug('ReadData CactiTHold: THold plugin not enabled. [THOLD002]\n');
-				return(FALSE);
+        			if(api_plugin_is_enabled('thold'))
+        			{
+					debug('ReadData CactiTHold: THold plugin not enabled. [THOLD002]\n');
+					return(FALSE);
+       				 }
+
 			}
-			
+			else
+			{		
+				if( !isset($plugins) || !in_array('thold',$plugins))
+				{
+					debug('ReadData CactiTHold: THold plugin not enabled. [THOLD002]\n');
+					return(FALSE);
+				}
+			}		
 			$sql = "show tables";
 			$result = db_fetch_assoc($sql) or die (mysql_error());
 			$tables = array();

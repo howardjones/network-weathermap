0c318ed0892ddcf17530e5f1f8d386a1d00fa061
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2010-02-12 14:38:49 +0000

    More test tweaks

--- trunk/test-suite/generate-test-references.sh
+++ trunk/test-suite/generate-test-references.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
-echo "<body bgcolor='#cccccc'>" > index.html
+DIRECTORY="references"
 
-for source in *.conf; do
+INDEX="index-$DIRECTORY.html"
+
+echo "<body bgcolor='#cccccc'>" > $INDEX
+
+for source in tests/*.conf; do
   destination=`basename $source`
-  destination="${destination}.png"
+  destination="$DIRECTORY/${destination}.png"
+
   echo "$source -> $destination"
-  echo "<hr>$source<br />" >> index.html
-  echo "<img src='${destination}'><br />" >> index.html
+  echo "<hr>$source<br />" >> $INDEX
+  echo "<img src='${destination}'><br />" >> $INDEX
   if [ ! -f $destination ]; then
-    ../weathermap --config $source --output $destination
+    php ../weathermap --config $source --output $destination
   else
    echo "Skipping (exists)"
   fi
 done
 
-echo "</body>" >> index.html
+echo "</body>" >> $INDEX

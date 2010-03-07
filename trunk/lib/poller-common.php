2151fce05b49a56222f787e26d8db1021a8cff60
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2010-03-07 09:26:26 +0000

    Fixed typo in poller-common.php. Made sure group 1 can't be deleted

--- trunk/lib/poller-common.php
+++ trunk/lib/poller-common.php
@@ -140,7 +140,7 @@ function weathermap_run_maps($mydir) {
 								$wmap->ReadConfig($mapfile);							
 
 								$wmap->add_hint("mapgroup",$map['groupname']);
-								$wmap->add_hint("mapgroupextra",($map['groupid'] ==1 ? "" : $map['groupname'] ));
+								$wmap->add_hint("mapgroupextra",($map['group_id'] ==1 ? "" : $map['groupname'] ));
 							
 								# in the order of precedence - global extras, group extras, and finally map extras
 								$queries = array();

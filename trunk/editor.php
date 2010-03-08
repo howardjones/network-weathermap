04de2e9c4e676b0bc43bfd7f6ae80842d97c93d8
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2010-03-08 21:15:48 +0000

    Docs recompile

--- trunk/editor.php
+++ trunk/editor.php
@@ -9,7 +9,7 @@ $ENABLED=false;
 if(! $ENABLED)
 {
     print "<p>The editor has not been enabled yet. You need to set ENABLED=true at the top of editor.php</p>";
-    print "<p>Before you do that, you should consider using FilesMatch (in Apache) or similar to limit who can access the editor.</p>";
+    print "<p>Before you do that, you should consider using FilesMatch (in Apache) or similar to limit who can access the editor. There is more information in the install guide section of the manual.</p>";
     exit();
 }
 

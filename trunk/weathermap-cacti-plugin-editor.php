f6c9860e7bf83b1486116d9ad3b7dcb8208757b9
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2010-03-10 21:52:26 +0000

    Forgot to add the new weathermap-cacti-plugin-editor.php Added the skeleton for the replacement picker too - this will use a server-side proxy to allow plugins to provide datasource browsing data. A new config file editor-collections.cfg will allow you to switch between multiple data sources.

---
+++ trunk/weathermap-cacti-plugin-editor.php
@@ -0,0 +1,9 @@
+<?php
+    chdir('../../');
+    include_once("./include/auth.php");
+    include_once("./include/config.php");
+
+    include_once($config["library_path"] . "/database.php");
+
+    include_once ("editor.php");
+?>

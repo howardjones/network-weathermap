bf64a2947c27443b879e232efe31d4e63f688cb5
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2010-04-06 23:23:48 +0000

    More tests!

--- trunk/WeatherMap.functions.php
+++ trunk/WeatherMap.functions.php
@@ -568,45 +568,6 @@ function imagecolorize($im, $r, $g, $b)
 }
 
 
-function RotX($a,$b,$x,$y)
-{
-	return  cos( atan2($y,$x) + atan2($b,$a) ) * sqrt( $x*$x + $y*$y );
-}
-
-function RotY($a,$b,$x,$y)
-{
-	return sin( atan2($y,$x) + atan2($b,$a) ) * sqrt( $x*$x + $y*$y ) ;
-}
-
-        function CalcArrow($x1,$y1,$x2,$y2,$w)
-        {
-            $poly = array();
-
-            $poly[] = $x1 + RotX($x2-$x1, $y2-$y1, 0, $w);
-            $poly[] = $y1 + RotY($x2-$x1, $y2-$y1, 0, $w);
-
-            $poly[] = $x2 + RotX($x2-$x1, $y2-$y1, -4*$w, $w);
-            $poly[] = $y2 + RotY($x2-$x1, $y2-$y1, -4*$w, $w);
-
-            $poly[] = $x2 + RotX($x2-$x1, $y2-$y1, -4*$w, 2*$w);
-            $poly[] = $y2 + RotY($x2-$x1, $y2-$y1, -4*$w, 2*$w);
-
-            $poly[] = $x2;
-            $poly[] = $y2;
-
-            $poly[] = $x2 + RotX($x2-$x1, $y2-$y1, -4*$w, -2*$w);
-            $poly[] = $y2 + RotY($x2-$x1, $y2-$y1, -4*$w, -2*$w);
-
-            $poly[] = $x2 + RotX($x2-$x1, $y2-$y1, -4*$w, -$w);
-            $poly[] = $y2 + RotY($x2-$x1, $y2-$y1, -4*$w, -$w);
-
-            $poly[] = $x1 + RotX($x2-$x1, $y2-$y1, 0, -$w);
-            $poly[] = $y1 + RotY($x2-$x1, $y2-$y1, 0, -$w);
-        
-			return $poly;
-		}
-
-
 
 
 

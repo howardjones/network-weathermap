44e711ccf86a97d3fa6287499581f35bd9bbca3f
Howard Jones <howardjones@users.noreply.github.com>
2013-03-28 17:29:25 +0000

    Remembered to add the new CSS for the manual Added the little test driver script Reset the editor_enabled flag to FALSE

---
+++ test.sh
@@ -0,0 +1,6 @@
+make test
+  grep  Output test-suite/diffs/*.txt | grep -v '|0|' | awk -F: '{ print $1;}' | sed -e 's/.png.txt//' -e 's/test-suite\/diffs\///' > test-suite/failing-images.txt
+        test-suite/make-failing-summary.pl test-suite/failing-images.txt test-suite/summary.html > test-suite/summary-failing.html
+
+echo
+echo "NOTE: 3 tests have a timestamp in them, and will always fail"

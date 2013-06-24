74ba6b3dc4c8f7e02d12b4a0c90271157f6b383c
howie <howie@efb71b59-4013-dc11-a8f3-0211060063e7>
2013-06-24 08:27:32 +0000

    More coding style fixups, and dead code removal.

--- trunk/weathermap-cacti-rebuild.php
+++ trunk/weathermap-cacti-rebuild.php
@@ -13,27 +13,21 @@
 $cacti_base = "../../";
 
 // check if the goalposts have moved
-if( is_dir($cacti_base) && file_exists($cacti_base."/include/global.php") )
-{
+if (is_dir($cacti_base) && file_exists($cacti_base."/include/global.php")) {
         // include the cacti-config, so we know about the database
-        require_once($cacti_base."/include/global.php");
-}
-elseif( is_dir($cacti_base) && file_exists($cacti_base."/include/config.php") )
-{
+        include_once $cacti_base."/include/global.php";
+} elseif (is_dir($cacti_base) && file_exists($cacti_base."/include/config.php")) {
         // include the cacti-config, so we know about the database
-        require_once($cacti_base."/include/config.php");
-}
-else
-{
-	print "Couldn't find a usable Cacti config";
+        include_once $cacti_base."/include/config.php";
+} else {
+	echo "Couldn't find a usable Cacti config";
 }
 
-require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."setup.php");
-require_once(dirname(__FILE__).DIRECTORY_SEPARATOR."lib".DIRECTORY_SEPARATOR."poller-common.php");
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR."setup.php";
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR."lib".DIRECTORY_SEPARATOR."poller-common.php";
 
 weathermap_setup_table();
 
-weathermap_run_maps(dirname(__FILE__) );
+weathermap_run_maps(dirname(__FILE__));
 
 // vim:ts=4:sw=4:
-?>

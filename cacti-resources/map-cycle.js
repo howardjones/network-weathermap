c029853fcb9befe61de3d4cdea6a3818f0ab8652
Howard Jones <Howard Jones>
2014-12-08 19:45:33 +0000

    more CC and jshint inspired cleanup for map-cycle.js

--- cacti-resources/map-cycle.js
+++ cacti-resources/map-cycle.js
@@ -1,17 +1,19 @@
+"use strict";
+/*global $:false */
+
 // http://stackoverflow.com/a/210733/2397542 - jQuery center function from Tony L.
 jQuery.fn.center = function () {
-    
     this.css("position", "fixed");
-    
+
   //  console.log( "(" + $(window).height() + " - " +  this.outerHeight() + ") / 2 + " +  $(window).scrollTop() );
-    
+
     this.css("top",  Math.max(0, (($(window).height() - this.outerHeight()) / 2) + $(window).scrollTop()) + "px");
     this.css("left", Math.max(0, (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft()) + "px");
-    
+
     return this;
 };
 
-WMcycler = {
+var WMcycler = {
 
     KEYCODE_ESCAPE : 27,
     KEYCODE_LEFT : 37,
@@ -56,8 +58,8 @@ WMcycler = {
         }
         wm_new = wm_new % this.nmaps;
 
-        var now = $(".weathermapholder").eq(this.current);
-        var next = $(".weathermapholder").eq(wm_new);
+        var now = $(".weathermapholder").eq(this.current),
+            next = $(".weathermapholder").eq(wm_new);
 
         if (this.fullscreen) {
             // in fullscreen, we centre everything, layer it with z-index and
@@ -69,11 +71,7 @@ WMcycler = {
             now.fadeOut(1200, function () {
                 // now that we're done with it, force a reload on the image just
                 // passed
-                var d = new Date();
-                var newurl = $(this).find('img').attr("src");
-                newurl = newurl.replace(/time=\d+/, "time=" + d.getTime());
-
-                $(this).find('img').attr("src", newurl);
+                this.forceReload();
             });
             next.fadeIn(1200);
         } else {
@@ -82,11 +80,7 @@ WMcycler = {
             now.hide(1, function () {
                 // now that we're done with it, force a reload on the image just
                 // passed
-                var d = new Date();
-                var newurl = $(this).find('img').attr("src");
-                newurl = newurl.replace(/time=\d+/, "time=" + d.getTime());
-
-                $(this).find('img').attr("src", newurl);
+                this.forceReload();
             });
             next.show(1);
         }
@@ -98,11 +92,20 @@ WMcycler = {
         this.updateProgress();
     },
 
-    hideControls: function() {
+    forceReload: function () {
+        var d = new Date(),
+            newurl = $(this).find('img').attr("src");
+
+        newurl = newurl.replace(/time=\d+/, "time=" + d.getTime());
+
+        $(this).find('img').attr("src", newurl);
+    },
+
+    hideControls: function () {
         $("#wmcyclecontrolbox").fadeOut(500);
     },
 
-    showControls: function() {
+    showControls: function () {
         $("#wmcyclecontrolbox").fadeIn(100);
     },
 
@@ -113,43 +116,13 @@ WMcycler = {
         this.fullscreen = initialData.fullscreen;
 
         this.nmaps = $(".weathermapholder").length;
+        $("#wm_total_map").text(this.nmaps);
 
         // copy of this that we can pass into callbacks
         var that = this;
 
-        $("#wm_total_map").text(this.nmaps);
-
-        $("#cycle_pause").click(function () {
-            that.pauseAction();
-        });
-        $("#cycle_next").click(function () {
-            that.nextAction();
-        });
-        $("#cycle_prev").click(function () {
-            that.previousAction();
-        });
-
-        $(document).keyup(function (event) {
-            if (event.keyCode === that.KEYCODE_ESCAPE) {
-                window.location.href = $('#cycle_stop').attr('href');
-                event.preventDefault();
-            }
-
-            if (event.keyCode === that.KEYCODE_SPACE) {
-                that.pauseAction();
-                event.preventDefault();
-            }
-            // left
-            if (event.keyCode === that.KEYCODE_LEFT) {
-                that.previousAction();
-                event.preventDefault();
-            }
-            // right
-            if (event.keyCode === that.KEYCODE_RIGHT) {
-                that.nextAction();
-                event.preventDefault();
-            }
-        });
+        this.initEvents(that);
+        this.initKeys(that);
 
         // stop here if there were no maps
         if (this.nmaps > 0) {
@@ -173,8 +146,49 @@ WMcycler = {
             this.timer_reloader = setTimeout(function () {
                 that.reload();
             }, this.poller_cycle);
-        }
 
+            this.initIdle(that);
+        }
+    },
+
+    initKeys: function (that) {
+        $(document).keyup(function (event) {
+            if (event.keyCode === that.KEYCODE_ESCAPE) {
+                window.location.href = $('#cycle_stop').attr('href');
+                event.preventDefault();
+            }
+
+            if (event.keyCode === that.KEYCODE_SPACE) {
+                that.pauseAction();
+                event.preventDefault();
+            }
+            // left
+            if (event.keyCode === that.KEYCODE_LEFT) {
+                that.previousAction();
+                event.preventDefault();
+            }
+            // right
+            if (event.keyCode === that.KEYCODE_RIGHT) {
+                that.nextAction();
+                event.preventDefault();
+            }
+        });
+    },
+
+    initEvents: function (that) {
+
+        $("#cycle_pause").click(function () {
+            that.pauseAction();
+        });
+        $("#cycle_next").click(function () {
+            that.nextAction();
+        });
+        $("#cycle_prev").click(function () {
+            that.previousAction();
+        });
+    },
+
+    initIdle: function (that) {
         // aim to get a video-player style OSD for fullscreen mode:
         // if the pointer is off the controls for more than 5 seconds, fade the
         // controls away
@@ -182,28 +196,20 @@ WMcycler = {
         // if the pointer is over the controls, don't fade
         if (this.fullscreen) {
 
-            $(document).idleTimer( {
+            $(document).idleTimer({
                 timeout: 5000
-            } );
+            });
 
-            $(document).on("idle.idleTimer", function(event, elem, obj){
+            $(document).on("idle.idleTimer", function () {
                 that.hideControls();
             });
-            $(document).on("active.idleTimer", function(event, elem, obj){
+            $(document).on("active.idleTimer", function () {
                 that.showControls();
             });
 
-            // this.hideControls();
-            // $("#wmcyclecontrolbox").delay(5000).fadeOut(500);
-            // $(document).mousemove( function ()
-            // {$("#wmcyclecontrolbox").fadeIn(100); } );
-            // $("#wmcyclecontrolbox").mouseleave( function () {
-            // $("#wmcyclecontrolbox").delay(2000).fadeOut(500); });
         }
     },
 
-    reloadAction : function () {
-    },
     nextAction : function () {
         this.switchMap(1);
     },

126cf64efe0de782ed63a8803666abe1ec3bcd13
Howard Jones <howie@thingy.com>
2015-05-19 08:26:21 +0100

    extracted the actual data reading in tabfile. Also added the regexps handled, and removed Recognise.

--- lib/datasources/WeatherMapDataSource_tabfile.php
+++ lib/datasources/WeatherMapDataSource_tabfile.php
@@ -7,13 +7,14 @@
 class WeatherMapDataSource_tabfile extends WeatherMapDataSource
 {
 
-    function Recognise($targetString)
+    public function __construct()
     {
-        if (preg_match('/\.(tsv|txt)$/', $targetString)) {
-            return true;
-        } else {
-            return false;
-        }
+        parent::__construct();
+
+        $this->regexpsHandled = array(
+            '/\.(tsv|txt)$/'
+        );
+
     }
 
     /**
@@ -22,7 +23,7 @@ class WeatherMapDataSource_tabfile extends WeatherMapDataSource
      * @param the $mapItem A reference to the object this target is attached to
      * @return array invalue, outvalue, unix timestamp that the data was valid
      */
-    function ReadData($targetString, &$map, &$mapItem)
+    public function ReadData($targetString, &$map, &$mapItem)
     {
         $data[IN] = null;
         $data[OUT] = null;
@@ -47,29 +48,46 @@ class WeatherMapDataSource_tabfile extends WeatherMapDataSource
         $fileHandle=fopen($fullpath, "r");
 
         if ($fileHandle) {
-            while (!feof($fileHandle)) {
-                $buffer=fgets($fileHandle, 4096);
-                # strip out any line-endings that have gotten in here
-                $buffer=str_replace("\r", "", $buffer);
-                $buffer=str_replace("\n", "", $buffer);
-
-                $parts = explode("\t", $buffer);
-
-                if ($parts[0] == $itemName) {
-                    $data[IN] = ($parts[1]=="-" ? null : WMUtility::interpretNumberWithMetricPrefix($parts[1]) );
-                    $data[OUT] = ($parts[2]=="-" ? null : WMUtility::interpretNumberWithMetricPrefix($parts[2]) );
-                }
-            }
-            $stats = stat($targetString);
+            $data = $this->readDataFromTSV($fileHandle, $itemName);
+            $stats = stat($fullpath);
             $dataTime = $stats['mtime'];
         } else {
             // some error code to go in here
-            wm_warn("TabText ReadData: Couldn't open ($targetString). [WMTABDATA01]\n");
+            wm_warn("TabText ReadData: Couldn't open ($fullpath). [WMTABDATA01]\n");
         }
 
         wm_debug("TabText ReadData: Returning (".($data[IN]===null ? 'null' : $data[IN]) . "," . ($data[OUT]===null ? 'null' : $data[OUT]).",$dataTime)\n");
 
         return( array($data[IN], $data[OUT], $dataTime) );
+    }
+
+    /**
+     * @param $fileHandle
+     * @param $itemName
+     * @return array
+     */
+    protected function readDataFromTSV($fileHandle, $itemName)
+    {
+        $data = array();
+        $data[IN] = null;
+        $data[OUT] = null;
+
+        while (!feof($fileHandle)) {
+            $buffer = fgets($fileHandle, 4096);
+
+            // strip out any line-endings that have gotten in here
+            $buffer = str_replace("\r", "", $buffer);
+            $buffer = str_replace("\n", "", $buffer);
+
+            $parts = explode("\t", $buffer);
+
+            if ($parts[0] == $itemName) {
+                $data[IN] = ($parts[1] == "-" ? null : WMUtility::interpretNumberWithMetricPrefix($parts[1]));
+                $data[OUT] = ($parts[2] == "-" ? null : WMUtility::interpretNumberWithMetricPrefix($parts[2]));
+            }
+        }
+
+        return $data;
     }
 }
 

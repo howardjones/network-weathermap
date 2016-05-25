da179b180ee6402ddc5db760694338e3cf258754
Howard Jones <howie@thingy.com>
2016-05-25 09:41:59 +0100

    removed md5 test - different versions of libpng produce different output

--- lib/Weathermap.class.php
+++ lib/Weathermap.class.php
@@ -1,6 +1,6 @@
 <?php
-// PHP Weathermap 0.97b
-// Copyright Howard Jones, 2005-2012 howie@thingy.com
+// PHP Weathermap 0.98
+// Copyright Howard Jones, 2005-2016 howie@thingy.com
 // http://www.network-weathermap.com/
 // Released under the GNU Public License
 
@@ -11,7 +11,7 @@ require_once "geometry.php";
 require_once "WeatherMapNode.class.php";
 require_once "WeatherMapLink.class.php";
 
-$WEATHERMAP_VERSION="0.97c";
+$WEATHERMAP_VERSION="0.98";
 $weathermap_debugging=FALSE;
 $weathermap_map="";
 $weathermap_warncount=0;

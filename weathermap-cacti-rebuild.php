d2fbe592bcec42b0213762d8d906817934a1cd46
Howard Jones <howie@thingy.com>
2017-03-18 19:38:47 +0000

    Fix for #66 - weathermap-cacti-rebuild.php actually runs with Cacti 1.0 now

--- weathermap-cacti-rebuild.php
+++ weathermap-cacti-rebuild.php
@@ -9,15 +9,17 @@
 // $cacti_base = "/XAMPP/htdocs/cacti-0.8.7e/";
 $cacti_base = dirname(__FILE__) . "/../../";
 
+$no_http_headers = true;
+
 // check if the goalposts have moved
 if (is_dir($cacti_base) && file_exists($cacti_base . "include/global.php")) {
-    // include the cacti-config, so we know about the database
-    require_once $cacti_base . "include/global.php";
+	// include the cacti-config, so we know about the database
+	require_once $cacti_base . "include/global.php";
 } elseif (is_dir($cacti_base) && file_exists($cacti_base . "include/config.php")) {
-    // include the cacti-config, so we know about the database
-    require_once $cacti_base . "include/config.php";
+	// include the cacti-config, so we know about the database
+	require_once $cacti_base . "include/config.php";
 } else {
-    print "Couldn't find a usable Cacti config";
+	print "Couldn't find a usable Cacti config";
 }
 
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "setup.php";

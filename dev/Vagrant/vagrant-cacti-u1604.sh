85867de8aea92f8ccfabb0d6fb30ef8b03c8f606
malgoe <martin@andelain.se>
2017-04-09 23:44:17 +0200

    Updated staging files for u1604 to work with php 5.6 and cacti 0.8.8h and also install weathermap on staging.

--- dev/Vagrant/vagrant-cacti-u1604.sh
+++ dev/Vagrant/vagrant-cacti-u1604.sh
@@ -1,9 +1,17 @@
+sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update -y
 ## For 'real' install:
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server-5.7 snmp rrdtool php5-cli php5-mysql apache2 libapache2-mod-php5 unzip php5-snmp php5-gd
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server-5.7 snmp rrdtool php7.0 php5.6 php5.6-common php5.6-cli php5.6-mysql apache2 libapache2-mod-php5.6 libapache2-mod-php7.0 unzip php5.6-snmp php5.6-gd php-gettext php5.6-mbstring php-xdebug unzip php5.6-xml
 # ## For dev/test, we need these too
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git subversion make xsltproc imagemagick zip curl phpunit
 #
+
+#Change to php 5.6
+sudo a2dismod php7.0
+sudo a2enmod php5.6
+sudo rm /etc/alternatives/php
+sudo ln -s /usr/bin/php5.6 /etc/alternatives/php
+sudo service apache2 restart
 
 # Get the common settings (CACTI_VERSION etc)
 . /vagrant/settings.sh
@@ -24,17 +32,21 @@ sudo tar --strip-components 1 --directory=${WEBROOT}/cacti -xvf /vagrant/cacti-$
 sudo chown -R cacti ${WEBROOT}/cacti/rra
 sudo chown -R cacti ${WEBROOT}/cacti/log
 
-mysql -uroot <<EOF
+sudo mysql -uroot <<EOF
+SET GLOBAL sql_mode = 'ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
 create database cacti;
 grant all on cacti.* to cactiuser@localhost identified by 'cactiuser';
 flush privileges;
 EOF
 
 if [ -f /vagrant/cacti-${CACTI_VERSION}-post-install.sql ]; then
-  mysql -uroot cacti < /vagrant/cacti-${CACTI_VERSION}-post-install.sql
+  sudo mysql -uroot cacti < /vagrant/cacti-${CACTI_VERSION}-post-install.sql
 else
-  mysql -uroot cacti < ${WEBROOT}/cacti/cacti.sql
+  sudo mysql -uroot cacti < ${WEBROOT}/cacti/cacti.sql
 fi
 
 sudo echo '# */5 * * * * cacti /usr/bin/php ${WEBROOT}/cacti/poller.php > ${WEBROOT}/last-cacti-poll.txt 2>&' > /etc/cron.d/cacti
 
+# Install Network Weathermap
+sudo unzip /network-weathermap/releases/php-weathermap-${WEATHERMAP_VERSION}.zip -d /var/www/html/cacti/plugins/
+sudo chown -R cacti ${WEBROOT}/cacti/plugins/weathermap

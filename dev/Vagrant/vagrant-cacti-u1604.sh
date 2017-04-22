079f3d0aed33ebfbc5ec3fcd4ce1bfdc582784c4
malgoe <martin@andelain.se>
2017-04-22 10:13:19 +0200

    Included Composer in u1604 vagrant

--- dev/Vagrant/vagrant-cacti-u1604.sh
+++ dev/Vagrant/vagrant-cacti-u1604.sh
@@ -1,9 +1,15 @@
+## TODO:
+# Add error handling. We don't want to strand a potential contributor just because one single package fails
+##
+
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update -y
 ## For 'real' install:
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server-5.7 snmp rrdtool php7.0 php5.6 php5.6-common php5.6-cli php5.6-mysql apache2 libapache2-mod-php5.6 libapache2-mod-php7.0 unzip php5.6-snmp php5.6-gd php-gettext php5.6-mbstring php-xdebug unzip php5.6-xml
 # ## For dev/test, we need these too
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git subversion make xsltproc imagemagick zip curl phpunit nodejs npm pandoc
+# ## For composer
+sudo DEBIAN_FRONTENT=noninteractive apt-get install -y php-mbstring php5.6-curl
 #
 
 #Install and run bower
@@ -11,6 +17,16 @@ sudo npm install -g bower
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 cd /network-weathermap
 bower install --allow-root
+
+#Install and run composer (this requires a swap partition for memory as well..)
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+sudo /sbin/mkswap /var/swap.1
+sudo /sbin/swapon /var/swap.1
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+cd /network-weathermap
+composer install
 
 #Change to php 5.6
 sudo a2dismod php7.0

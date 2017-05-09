2d5598ba8647bb3a315c8181fec247f40fdb6c0d
Howard Jones <howie@thingy.com>
2017-05-09 17:36:11 +0100

    (untested) change to 'defend' against Cacti installer loading setup.php without $config defined

--- setup.php
+++ setup.php
@@ -1,11 +1,13 @@
 <?php
 
+// if weathermap is in the plugins folder while the Cacti installer is doing its thing,
+// then it loads this setup.php but without the normal cacti $config defined, which breaks things.
+if (isset($config) && array_key_exists($config, "cacti_version")) {
+    if (substr($config['cacti_version'], 0, 3) == "0.8") {
+        require_once "setup88.php";
+    }
 
-
-if (substr($config['cacti_version'], 0, 3) == "0.8") {
-    require_once "setup88.php";
-}
-
-if (substr($config['cacti_version'], 0, 2) == "1.") {
-    require_once "setup10.php";
+    if (substr($config['cacti_version'], 0, 2) == "1.") {
+        require_once "setup10.php";
+    }
 }
